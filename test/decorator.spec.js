7dc55e7ee5321ce64b572c9332a8217226bb550d
Leonardo Thibes <leonardothibes@gmail.com>
2017-10-23 10:57:32 -0200

    Implementando teste do decorator

---
+++ test/decorator.spec.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const decorator = require('../lib/decorator'),
+      assert    = require('unit.js');
+
+describe('Decorator', function()
+{
+    it('Mask String', function(done)
+    {
+        assert.string(decorator.mask('12345678912')).isEqualTo('123.456.789-12');
+        done();
+    });
+
+    it('Mask Number', function(done)
+    {
+        assert.string(decorator.mask(12345678912)).isEqualTo('123.456.789-12');
+        done();
+    });
+
+    it('Mask Fail', function(done)
+    {
+        assert.string(decorator.mask('123')).isEqualTo('123');
+        done();
+    });
+
+    it('UnMask Masked', function(done)
+    {
+        assert.string(decorator.unMask('123.456.789-12')).isEqualTo('12345678912');
+        done();
+    });
+
+    it('UnMask UnMasked', function(done)
+    {
+        assert.string(decorator.unMask('12345678912')).isEqualTo('12345678912');
+        done();
+    });
+});
