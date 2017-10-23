b5d76452ef9da28f4ef76a01b87ccce6568fb6b3
Leonardo Thibes <leonardothibes@gmail.com>
2017-10-23 14:53:42 -0200

    Escrevendo teste para o Entry Point da biblioteca

---
+++ test/index.spec.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const cpf    = require('../index'),
+      assert = require('unit.js');
+
+describe('Entry Point', function()
+{
+    it('Validate Masked Valid', function(done)
+    {
+        let valid = cpf.validate('821.649.079-81');
+        assert.bool(valid).isTrue();
+        done();
+    });
+
+    it('Validate Masked Invalid', function(done)
+    {
+        let valid = cpf.validate('821.649.079-82');
+        assert.bool(valid).isFalse();
+        done();
+    });
+
+    it('Validate UnMasked Valid', function(done)
+    {
+        let valid = cpf.validate('82164907981');
+        assert.bool(valid).isTrue();
+        done();
+    });
+
+    it('Validate UnMasked Invalid', function(done)
+    {
+        let valid = cpf.validate('82164907982');
+        assert.bool(valid).isFalse();
+        done();
+    });
+
+    it('Mask', function(done)
+    {
+        let masked = cpf.mask('23202006223');
+        assert.string(masked).isEqualTo('232.020.062-23');
+        done();
+    });
+
+    it('UnMask', function(done)
+    {
+        let unMasked = cpf.unMask('232.020.062-23');
+        assert.string(unMasked).isEqualTo('23202006223');
+        done();
+    });
+
+    it('Generate UnMasked', function(done)
+    {
+        let generated = cpf.generate();
+        assert.bool(cpf.validate(generated)).isTrue();
+        done();
+    });
+
+    it('Generate Masked', function(done)
+    {
+        let generated = cpf.generate(true);
+        let isMasked  = new RegExp(/[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}/);
+
+        assert.bool(cpf.validate(generated)).isTrue();
+        assert.bool(isMasked.test(generated)).isTrue();
+
+        done();
+    });
+});
